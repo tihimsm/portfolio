4a4ec8fb16f5d3b35457de1fbe7a83a82d01900f
三島 太平 <mishimataihei@LAB-N1009.local>
2016-10-14 16:02:42 +0900

    雛形追加

---
+++ css/config.rb
@@ -0,0 +1,8 @@
+http_path = "../"
+css_dir = "/"
+sass_dir = "scss"
+images_dir = "../img"
+javascripts_dir = "../js"
+line_comments = false
+cache = false
+asset_cache_buster :none
---
+++ js/import.js
@@ -0,0 +1,8 @@
+document.write('<script src="js/jquery.min.js"></script>');
+document.write('<script src="js/jquery.transit.js"></script>');
+
+// if ( window.innerWidth > 771) {
+  document.write('<script src="js/main.js"></script>');
+// } else {
+//   document.write('<script src="js/smart.js"></script>');
+// }
