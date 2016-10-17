b804cb0d0b130f99774cc14bbb65071623d1d207
三島 太平 <mishimataihei@LAB-N1009.local>
2016-10-17 16:43:05 +0900

    一時コミット

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(window).load(function(){
-
+  // countDown();
 });
+function countDown() {
+  var startDateTime = new Date();
+  var endDateTime = new Date("January 01, 2017 00:00:00");
+  var left = endDateTime - startDateTime;
+  var a_day = 24 * 60 * 60 * 1000;
+
+  var d = Math.floor(left / a_day);
+  var h = Math.floor((left % a_day) / (60 * 60 * 1000));
+  var m = Math.floor((left % a_day) / (60 * 1000)) % 60;
+  var s = Math.floor((left % a_day) / 1000) % 60 % 60;
+
+  $("#timeLeft p").text(d + '日' + h + '時間' + m + '分' + s + '秒');
+  setTimeout('countDown()', 1000);
+}
